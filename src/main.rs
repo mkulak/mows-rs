1297b5ac857f52897631eded3823b8df7c95b532
mkulak <kleshney@gmail.com>
2021-05-02 23:38:47 +0200

    websocket with path checking

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+use std::{result::Result as StdResult};
+use std::net::SocketAddr;
+
+use futures_util::{SinkExt, StreamExt};
+use log::*;
+use serde::{Deserialize, Serialize};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_hdr_async, tungstenite::Error};
+use tokio_tungstenite::tungstenite::handshake::client::Request;
+use tokio_tungstenite::tungstenite::handshake::server::{Callback, ErrorResponse, Response};
+use tungstenite::{http, Result};
+
+async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
+    if let Err(e) = handle_connection(peer, stream).await {
+        match e {
+            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+            err => error!("Error processing connection: {}", err),
+        }
+    }
+}
+
+struct SimpleCallback {
+    path: String,
+}
+
+impl Callback for SimpleCallback {
+    fn on_request(self, request: &Request, response: Response) -> StdResult<Response, ErrorResponse> {
+        if request.uri().path() != self.path {
+            return Err(http::response::Response::builder()
+                .status(http::StatusCode::NOT_FOUND)
+                .body(None)
+                .unwrap());
+        }
+        Ok(response)
+    }
+}
+
+async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
+    let cb = SimpleCallback {
+        path: "/rooms".to_owned()
+    };
+    let mut ws_stream = accept_hdr_async(stream, cb).await.expect("Failed to accept");
+    let id = "123".to_owned();
+    let msg = LoginServerMessage { id, _type: "login".to_owned() };
+    let data = serde_json::to_string(&msg).unwrap();
+    ws_stream.send(data.into()).await?;
+    info!("New WebSocket connection: {}", peer);
+
+    while let Some(msg) = ws_stream.next().await {
+        let msg = msg?;
+        if msg.is_text() || msg.is_binary() {
+            ws_stream.send(msg).await?;
+        }
+    }
+
+    Ok(())
+}
+
+
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let addr = "127.0.0.1:9002";
+    let listener = TcpListener::bind(&addr).await.expect("Can't listen");
+    info!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let peer = stream.peer_addr().expect("connected streams should have a peer address");
+        info!("Peer address: {}", peer);
+
+        tokio::spawn(accept_connection(peer, stream));
+    }
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LoginServerMessage {
+    pub id: String,
+    #[serde(rename = "type")]
+    pub _type: String,
+}
