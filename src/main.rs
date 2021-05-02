84f18e3247e8090ab72215c8b08bb821d6d851fa
mkulak <kleshney@gmail.com>
2021-05-03 00:34:27 +0200

    introduce space server

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,56 @@ use tokio_tungstenite::{accept_hdr_async, tungstenite::Error};
 use tokio_tungstenite::tungstenite::handshake::client::Request;
 use tokio_tungstenite::tungstenite::handshake::server::{Callback, ErrorResponse, Response};
 use tungstenite::{http, Result};
+use std::collections::HashMap;
 
-async fn accept_connection(peer: SocketAddr, stream: TcpStream) {
-    if let Err(e) = handle_connection(peer, stream).await {
-        match e {
-            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
-            err => error!("Error processing connection: {}", err),
+struct Room {
+    id: String,
+    next_user_id: u32,
+}
+
+struct SpaceServer {
+    rooms: HashMap<String, Room>
+}
+
+impl SpaceServer {
+    async fn accept_connection(self, peer: SocketAddr, stream: TcpStream) {
+        if let Err(e) = self.handle_connection(peer, stream).await {
+            match e {
+                Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+                err => error!("Error processing connection: {}", err),
+            }
         }
     }
+
+    async fn handle_connection(mut self, peer: SocketAddr, stream: TcpStream) -> Result<()> {
+        let mut cb = PathCapturingCallback {
+            path: String::new()
+        };
+        let mut ws_stream = accept_hdr_async(stream, &mut cb).await?;
+        let room_id = cb.path[ROOMS_PREFIX.len()..].to_owned();
+        info!("New WebSocket connection: {} {}", peer, room_id);
+        let room = self.rooms.entry(room_id.clone())
+            .or_insert_with(|| Room { id: room_id.clone(), next_user_id: 0});
+        let id = room.next_user_id.to_string();
+        room.next_user_id += 1;
+        let msg = LoginServerMessage { id, _type: "login".to_owned() };
+        let data = serde_json::to_string(&msg).unwrap();
+        ws_stream.send(data.into()).await?;
+
+        while let Some(msg) = ws_stream.next().await {
+            let msg = msg?;
+            if msg.is_text() || msg.is_binary() {
+                ws_stream.send(msg).await?;
+            }
+        }
+
+        Ok(())
+    }
+
+
 }
+
+const ROOMS_PREFIX: &'static str = "/rooms/";
 
 struct PathCapturingCallback {
     path: String,
@@ -26,7 +67,7 @@ struct PathCapturingCallback {
 impl Callback for &mut PathCapturingCallback {
     fn on_request(self, request: &Request, response: Response) -> StdResult<Response, ErrorResponse> {
         let path = request.uri().path();
-        if !path.starts_with("/rooms/") {
+        if !path.starts_with(ROOMS_PREFIX) {
             return Err(http::response::Response::builder()
                 .status(http::StatusCode::NOT_FOUND)
                 .body(None)
@@ -37,26 +78,7 @@ impl Callback for &mut PathCapturingCallback {
     }
 }
 
-async fn handle_connection(peer: SocketAddr, stream: TcpStream) -> Result<()> {
-    let mut cb = PathCapturingCallback {
-        path: String::new()
-    };
-    let mut ws_stream = accept_hdr_async(stream, &mut cb).await?;
-    let id = "123".to_owned();
-    let msg = LoginServerMessage { id, _type: "login".to_owned() };
-    let data = serde_json::to_string(&msg).unwrap();
-    ws_stream.send(data.into()).await?;
-    info!("New WebSocket connection: {} {}", peer, cb.path);
 
-    while let Some(msg) = ws_stream.next().await {
-        let msg = msg?;
-        if msg.is_text() || msg.is_binary() {
-            ws_stream.send(msg).await?;
-        }
-    }
-
-    Ok(())
-}
 
 
 #[tokio::main]
@@ -70,8 +92,11 @@ async fn main() {
     while let Ok((stream, _)) = listener.accept().await {
         let peer = stream.peer_addr().expect("connected streams should have a peer address");
         info!("Peer address: {}", peer);
+        let spaces_server = SpaceServer {
+            rooms: HashMap::new()
+        };
 
-        tokio::spawn(accept_connection(peer, stream));
+        tokio::spawn(spaces_server.accept_connection(peer, stream));
     }
 }
 
@@ -82,3 +107,4 @@ pub struct LoginServerMessage {
     #[serde(rename = "type")]
     pub _type: String,
 }
+
