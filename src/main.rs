fea43e9753ca92cbb5b5f4b81d312afdc1237c0f
mkulak <kleshney@gmail.com>
2021-05-07 23:47:47 +0200

    add participatns and clients

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
-use std::result::Result as StdResult;
 use std::borrow::BorrowMut;
 use std::cell::RefCell;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::net::SocketAddr;
 use std::rc::Rc;
-use std::sync::{Arc};
+use std::result::Result as StdResult;
+use std::sync::Arc;
+use std::thread;
 
 use futures_util::{SinkExt, StreamExt};
 use log::*;
+use parking_lot::Mutex;
 use serde::{Deserialize, Serialize};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::{accept_hdr_async, tungstenite::Error};
+use tokio_tungstenite::{accept_hdr_async, tungstenite::Error, WebSocketStream};
 use tokio_tungstenite::tungstenite::handshake::client::Request;
 use tokio_tungstenite::tungstenite::handshake::server::{Callback, ErrorResponse, Response};
 use tungstenite::{http, Result};
-use std::thread;
-use parking_lot::Mutex;
 
+use rand::{Rng, thread_rng};
+use rand::distributions::Alphanumeric;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -26,7 +28,7 @@ async fn main() {
     let listener = TcpListener::bind(&addr).await.expect("Can't listen");
     info!("Listening on: {}", addr);
 
-    let spaces_server = Arc::new(Mutex::new(SpaceServer { rooms: HashMap::new() }));
+    let spaces_server = Arc::new(Mutex::new(create_spaces_state()));
     while let Ok((stream, _)) = listener.accept().await {
         let peer = stream.peer_addr().expect("connected streams should have a peer address");
         info!("Peer address: {}", peer);
@@ -35,16 +37,7 @@ async fn main() {
     }
 }
 
-struct Room {
-    id: String,
-    next_user_id: u32,
-}
-
-struct SpaceServer {
-    rooms: HashMap<String, Room>,
-}
-
-async fn accept_connection(mut space: Arc<Mutex<SpaceServer>>, peer: SocketAddr, stream: TcpStream) {
+async fn accept_connection(mut space: Arc<Mutex<SpacesState>>, peer: SocketAddr, stream: TcpStream) {
     if let Err(e) = handle_connection(space, peer, stream).await {
         match e {
             Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
@@ -53,7 +46,7 @@ async fn accept_connection(mut space: Arc<Mutex<SpaceServer>>, peer: SocketAddr,
     }
 }
 
-async fn handle_connection(mut space: Arc<Mutex<SpaceServer>>, peer: SocketAddr, stream: TcpStream) -> Result<()> {
+async fn handle_connection(mut space: Arc<Mutex<SpacesState>>, peer: SocketAddr, stream: TcpStream) -> Result<()> {
     let mut cb = PathCapturingCallback {
         path: String::new()
     };
@@ -62,10 +55,10 @@ async fn handle_connection(mut space: Arc<Mutex<SpaceServer>>, peer: SocketAddr,
     info!("New WebSocket connection: {} {}", peer, room_id);
     let id = {
         let mut s = space.lock();
-        let room = s.rooms.entry(room_id.clone())
-            .or_insert_with(|| Room { id: room_id.clone(), next_user_id: 0 });
-        let id = room.next_user_id.to_string();
-        room.next_user_id += 1;
+        let id = s.next_player_id.to_string();
+        s.next_player_id += 1;
+        let room = s.rooms.entry(room_id.clone()).or_insert_with(|| create_room(&room_id));
+        room.participants.insert(id.clone());
         id
     };
     let msg = LoginServerMessage { id, _type: "login".to_owned() };
@@ -74,13 +67,37 @@ async fn handle_connection(mut space: Arc<Mutex<SpaceServer>>, peer: SocketAddr,
 
     while let Some(msg) = ws_stream.next().await {
         let msg = msg?;
-        if msg.is_text() || msg.is_binary() {
+        if msg.is_text() {
+            // let serde_json::from_slice(msg.into());
+            info!("got: {}", msg.clone().into_text().unwrap());
             ws_stream.send(msg).await?;
         }
     }
 
     Ok(())
 }
+
+fn create_spaces_state() -> SpacesState {
+    SpacesState { rooms: HashMap::new(), clients: HashMap::new(), next_player_id: 0 }
+}
+
+fn create_room(room_id: &String) -> Room {
+    Room { id: room_id.clone(), participants: HashSet::new() }
+}
+
+struct Room {
+    id: String,
+    participants: HashSet<PlayerId>
+}
+
+struct SpacesState {
+    rooms: HashMap<RoomId, Room>,
+    clients: HashMap<PlayerId, WebSocketStream<TcpStream>>,
+    next_player_id: u32,
+}
+type RoomId = String;
+
+type PlayerId = String;
 
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -108,4 +125,12 @@ impl Callback for &mut PathCapturingCallback {
         (&mut self.path).push_str(path);
         Ok(response)
     }
+}
+
+fn nextId() -> String {
+    return thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(22)
+        .map(char::from)
+        .collect();
 }
